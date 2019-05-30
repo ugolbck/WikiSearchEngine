b1da1cdf241380a5158baca60dd8f3cf69338e0b
Ugo Loobuyck <ugo813@hotmail.fr>
2019-05-30 11:56:44 +0200

    docsToDict function 1.0

--- generator.py
+++ generator.py
@@ -1,3 +1,29 @@
 ''' We parse each document of each file from our data set '''
-''' This results in a dictionary docID:[title, url, content] '''
+''' This results in a dictionary docID:content '''
 
+
+def docsToDict(fname):
+
+    docs = {}
+    backup = {}
+    with open(fname) as in_file:
+        lines = [[line.rstrip('\n')] for line in in_file]
+    
+    position = 0
+    for line in lines:
+        # print(line)
+        if '<doc id=' in line[0]:
+            elements = line[0].split()
+            docID = elements[1][4:-1]
+            url = elements[2][5:-1]
+            title = elements[3][7:]
+
+            docs[docID] = []
+            backup[docID] = [title, url]
+        elif '</doc>' in line[0]:
+            docID = None
+        else:
+            if docID and line[0] != '':
+                docs[docID].extend(line) 
+
+docsToDict('wiki_01')
