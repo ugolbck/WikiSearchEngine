4ec9e697973a112f0c534f5adaeccaad58f0b3bf
Ugo Loobuyck <ugo813@hotmail.fr>
2019-05-30 18:43:43 +0200

    docsToFiles v3.0 + vector_index v1.0

--- generator.py
+++ generator.py
@@ -1,11 +1,12 @@
 ''' We parse each document of each file from our data set '''
-''' This results in a dictionary docID:content '''
+''' This results in text files with ID name and text content '''
 
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 import nltk
 import re
 
-# Turns xml parsed file into dictionary of document contents
+# Turns xml parsed file into separate documents
 def docsToFiles(fname, language):
 
     backup = {}
@@ -23,7 +24,7 @@ def docsToFiles(fname, language):
             backup[docID] = [title, url]
         elif '</doc>' in line[0]:
             with open(folder_path + docID + '.txt', 'w') as f:
-                f.write(str(flt))
+                f.write(str(flt)[1:-1])
             docID = None
         else:
             if docID and line[0] != '':
@@ -31,14 +32,39 @@ def docsToFiles(fname, language):
                 sl.extend(line)
                 s = re.sub(r'[^\w\s]', '', sl[0])
                 tokens = nltk.word_tokenize(s.lower())
-                # print(tokens)
                 new = [w for w in tokens if not w in stopwords.words(language)]
-                flt.extend(new)
+                stemmer = PorterStemmer()
+                final = [stemmer.stem(word) for word in new]
+                flt.extend(final)
                 
     return backup
 
+
+def vector_index(backup):
+    vector_dic = {}
+    Ndic = {}
+    folder_path = './DOCUMENTS/'
+    for k in backup.keys():
+        docID = k
+        with open(folder_path + docID + '.txt', 'r') as f:
+            line = f.read().split(', ')
+        Ndic[docID] = len(line)
+        for token in line:
+            if token not in vector_dic.keys():
+                vector_dic[token] = {docID: 1}
+            else:
+                if docID in vector_dic[token].keys():
+                    vector_dic[token][docID] += 1
+                else:
+                    vector_dic[token][docID] = 1
+               
+    return Ndic, vector_dic
         
 
 if __name__ == '__main__':
-    backup = docsToFiles('wiki_01', 'french')
+    backup = docsToFiles('wiki_en', 'english')
+    lenghts, dico = vector_index(backup)
+
+    print(dico)
+    
     
