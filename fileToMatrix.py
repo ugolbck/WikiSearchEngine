f84997655267ae27ac3d127dd12a1d59be7b3125
Ugo Loobuyck <ugo813@hotmail.fr>
2019-05-31 15:24:45 +0200

    files and functions

---
+++ fileToMatrix.py
@@ -0,0 +1,8 @@
+import numpy
+from numpy import zeros
+import numpy
+
+
+matrix = numpy.loadtxt('space.txt')
+
+print(matrix.size) 
--- generator.py
+++ generator.py
@@ -5,6 +5,9 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import nltk
 import re
+from numpy import zeros
+import numpy as np
+import math
 
 # Turns xml parsed file into separate documents
 def docsToFiles(fname, language):
@@ -24,7 +27,8 @@ def docsToFiles(fname, language):
             backup[docID] = [title, url]
         elif '</doc>' in line[0]:
             with open(folder_path + docID + '.txt', 'w') as f:
-                f.write(str(flt)[1:-1])
+                flt = re.sub(r'[^\w\s]', '', str(flt))
+                f.write(flt)
             docID = None
         else:
             if docID and line[0] != '':
@@ -40,31 +44,66 @@ def docsToFiles(fname, language):
     return backup
 
 
-def vector_index(backup):
-    vector_dic = {}
+def indexing(backup):
+    index_dic = {}
     Ndic = {}
+    posDic = {}
+    position = 0
     folder_path = './DOCUMENTS/'
+    
     for k in backup.keys():
         docID = k
         with open(folder_path + docID + '.txt', 'r') as f:
-            line = f.read().split(', ')
+            line = f.read().split(' ')
         Ndic[docID] = len(line)
         for token in line:
-            if token not in vector_dic.keys():
-                vector_dic[token] = {docID: 1}
+            if token not in index_dic.keys():
+                index_dic[token] = {docID: 1}
+                posDic[token] = position
+                position += 1
             else:
-                if docID in vector_dic[token].keys():
-                    vector_dic[token][docID] += 1
+                if docID in index_dic[token].keys():
+                    index_dic[token][docID] += 1
                 else:
-                    vector_dic[token][docID] = 1
-               
-    return Ndic, vector_dic
+                    index_dic[token][docID] = 1
+    
+    return Ndic, index_dic, posDic
+
+
+# Creation of a vector space (matrix) containing all documents vectors
+def doc_vector(ind, back, pos, docLen):
+    mapping = {}
+    matrix = zeros(shape=(len(back.keys()), len(ind.keys())))
+    pointer = 0
+    for i in back.keys():
+        file_words = set()
+        for k, v in ind.items():
+            if i in v.keys():
+                file_words.add(k)
+        for j in file_words:
+            tf = ind[j][i] / docLen[i]
+            idf = math.log(len(back.keys())/(len(ind[j])))
+            matrix[pointer][pos[j]] = tf * idf
+            mapping[i] = pointer
+        pointer += 1
+    with open('vectorSpace.txt', 'w') as vec, open('mapping.txt', 'w') as mapp:
+            np.savetxt(vec, matrix, fmt='%.6f')
+            for key, val in mapping.items():
+                docID, row = str(key), str(val)
+                mapp.write(docID + ' ' + row + '\n')
+    
+        
+    
+        
+
         
 
 if __name__ == '__main__':
     backup = docsToFiles('wiki_en', 'english')
-    lenghts, dico = vector_index(backup)
+    docLenghts, index, positions = indexing(backup)
+    doc_vector(index, backup, positions, docLenghts)
 
-    print(dico)
+
+    # print(backup)
     
     
